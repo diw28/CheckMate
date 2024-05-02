598fc849a0531b142f9840ca69a745418cdeb3cd
diw28 <diw28@dimigo.hs.kr>
2024-05-02 22:26:12 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-import 'screens/home_screen.dart';
 import 'screens/splash_screen.dart';
-import 'screens/tutorial_screen.dart';
-
 import 'themes/color_theme.dart';
 
 void main() {
